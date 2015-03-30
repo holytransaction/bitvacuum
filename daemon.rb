65c97da8c43ed11c990de595332ba962aa00f717
Dzmitry <stormdjent@gmail.com>
2015-03-30 14:44:54 +0300

    Daemon loop fix

--- daemon.rb
+++ daemon.rb
@@ -2,6 +2,8 @@ require 'Open3'
 require 'yaml'
 
 class Daemon
-  system('cd ' +File.expand_path('..',  __FILE__) + ';bundle exec bin/bitvacuum.rb ' + ARGV.join(" "))
-  sleep(YAML::load_file(File.join(__dir__, 'daemon.yml'))['operation_interval'] * 60)
+  loop do
+    system('cd ' +File.expand_path('..',  __FILE__) + ';bundle exec bin/bitvacuum.rb ' + ARGV.join(" "))
+    sleep(YAML::load_file(File.join(__dir__, 'daemon.yml'))['operation_interval'] * 60)
+  end
 end
