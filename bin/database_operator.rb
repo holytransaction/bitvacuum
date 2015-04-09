9834c3df9ab54140d4caddf2bed3d50e7e4723a1
Dzmitry <stormdjent@gmail.com>
2015-04-09 16:34:37 +0300

    HT-254 Separate db for test and dev

--- bin/database_operator.rb
+++ bin/database_operator.rb
@@ -3,7 +3,13 @@ require 'logger'
 require 'mysql2'
 require 'yaml'
 
-ActiveRecord::Base.establish_connection YAML::load_file(File.join(__dir__, '../config/database.yml'))['development']
+config_file = YAML::load_file(File.join(__dir__, '../config/database.yml'))
+if ENV['RACK_ENV'] == 'test'
+  ActiveRecord::Base.establish_connection config_file['development']
+else
+  ActiveRecord::Base.establish_connection config_file['test']
+end
+
 I18n.enforce_available_locales = false
 
 class DatabaseOperator
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 
+ENV['RACK_ENV'] = 'test'
+
 RSpec.configure do |config|
   config.color = true
 
